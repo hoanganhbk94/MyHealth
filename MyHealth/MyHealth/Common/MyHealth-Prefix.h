8b07ce5afbe75d4c3bef2da73c222eabd02819f6
Anh Mai <anh.mai@eastgate-software.com>
2018-05-07 15:00:31 +0700

    + Add bridge header file

---
+++ MyHealth/MyHealth/Common/MyHealth-Prefix.h
@@ -0,0 +1,13 @@
+//
+//  MyHealth-Prefix.h
+//  MyHealth
+//
+//  Created by Mai Hoang Anh on 5/7/18.
+//  Copyright © 2018 Mai Hoang Anh. All rights reserved.
+//
+
+#ifndef MyHealth_Prefix_h
+#define MyHealth_Prefix_h
+
+#import <Library/Library.h>
+#endif /* MyHealth_Prefix_h */
